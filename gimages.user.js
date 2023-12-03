f398bf23dab4de7dc73b6da30b3c92a125806afb
Ssieth Anabuki <38430835+Ssieth@users.noreply.github.com>
2023-12-03 19:35:05 +0000

    v0.2.1
    Bug fix

--- gimages.user.js
+++ gimages.user.js
@@ -11,7 +11,7 @@
 //
 // @require     https://code.jquery.com/jquery-3.7.1.min.js
 //
-// @version     0.2.0
+// @version     0.2.1
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_deleteValue
@@ -21,22 +21,25 @@
 // @grant       GM_xmlhttpRequest
 // @grant       GM_getResourceURL
 // @license     GPL
+// @copyright   2023, Ssieth
 // ==/UserScript==
 
 /*jshint esversion: 6 */
 /* jshint -W083 */
 
-
-function updateURLs_img() {
-  $img = $("a[href^='http'] > img[style^='max-width:']").eq(0);
-  if ($img.length > 0) {
-    $a = $img.parent();
-    if ($a.attr("ssi-proc") !== 'done') {
-      $a.attr("ssi-proc","done");
-      let src = $img.attr("src")
-      $a.attr("href",src);
+function updateURLs_img2() {
+  $imgs = $("img[style^='max-width:']");
+  $imgs.each(function() {
+    $img = $(this);
+    let href = $img.attr("src");
+    if ($img.attr("ssi-proc") !== 'done') {
+      $img.click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        window.open(href, '_blank').focus();
+      })
     }
-  }
+  });
 }
 
 function updateURLs_siteLink() {
@@ -58,7 +61,7 @@ function updateURLs_siteLink() {
 }
 
 function updateURLs() {
-  updateURLs_img();
+  updateURLs_img2();
   updateURLs_siteLink();
 }
 
@@ -66,68 +69,68 @@ function tick() {
   updateURLs();
 }
 
-function showDims() {
-	// Find all thumbnails & exclude the "already handled" class we set below
-	const images = document.querySelectorAll('[data-ow]:not(.img-dims):not([data-ismultirow])');
+  function showDims() {
+    // Find all thumbnails & exclude the "already handled" class we set below
+    const images = document.querySelectorAll('[data-ow]:not(.img-dims):not([data-ismultirow])');
 
-	// Loop through all thumbnails
-	images.forEach((image) => {
-	  try {
-		// Get original width from 'data-ow' attribute
-		const width = image.getAttribute('data-ow');
+    // Loop through all thumbnails
+    images.forEach((image) => {
+      try {
+        // Get original width from 'data-ow' attribute
+        const width = image.getAttribute('data-ow');
 
-		// Get original height from 'data-oh' attribute
-		const height = image.getAttribute('data-oh');
+        // Get original height from 'data-oh' attribute
+        const height = image.getAttribute('data-oh');
 
-		// Create p tag and insert text
-		const dimensionsDiv = document.createElement('p');
-		const dimensionsContent = document.createTextNode(width + ' × ' + height);
-		dimensionsDiv.appendChild(dimensionsContent);
+        // Create p tag and insert text
+        const dimensionsDiv = document.createElement('p');
+        const dimensionsContent = document.createTextNode(width + ' × ' + height);
+        dimensionsDiv.appendChild(dimensionsContent);
 
-		// Append everything to thumbnail
-		image.children[1].appendChild(dimensionsDiv);
+        // Append everything to thumbnail
+        image.children[1].appendChild(dimensionsDiv);
 
-		// Add CSS class to the thumbnail
-		image.classList.add('img-dims');
+        // Add CSS class to the thumbnail
+        image.classList.add('img-dims');
 
-	  } catch (error) {
-		console.error(error);
-	  }
-	});
-}
+      } catch (error) {
+        console.error(error);
+      }
+    });
+  }
 
-// Initialize new MutationObserver
-const mutationObserver = new MutationObserver(showDims);
+  // Initialize new MutationObserver
+  const mutationObserver = new MutationObserver(showDims);
 
-function addGlobalStyle(css) {
-	const head = document.getElementsByTagName('head')[0];
-	if (!head) { return; }
-	const style = document.createElement('style');
-	style.textContent = css;
-	head.appendChild(style);
-}
+  function addGlobalStyle(css) {
+    const head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    const style = document.createElement('style');
+    style.textContent = css;
+    head.appendChild(style);
+  }
 
 $(document).ready( function() {
      // Add Google's own CSS used for image dimensions
-    addGlobalStyle(`
-      .img-dims p {
-        position: absolute;
-        bottom: 0;
-        right: 0;
-        margin: 0;
-        padding: 4px;
-        color: #f1f3f4;
-        background-color: rgba(0,0,0,.5);
-        border-radius: 2px 0 0 0;
-        font-family: Roboto-Medium,Roboto,Arial,sans-serif;
-        font-size: 10px;
-        line-height: 12px;
-      }
-    `);
-    const targetNode = document.querySelector('div[data-cid="GRID_STATE0"]');
-    showDims();
-    mutationObserver.observe(targetNode, { childList: true, subtree: true });
-    setInterval(function () {
+  addGlobalStyle(`
+    .img-dims p {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      margin: 0;
+      padding: 4px;
+      color: #f1f3f4;
+      background-color: rgba(0,0,0,.5);
+      border-radius: 2px 0 0 0;
+      font-family: Roboto-Medium,Roboto,Arial,sans-serif;
+      font-size: 10px;
+      line-height: 12px;
+    }
+  `);
+  const targetNode = document.querySelector('div[data-cid="GRID_STATE0"]');
+  showDims();
+  mutationObserver.observe(targetNode, { childList: true, subtree: true });
+  setInterval(function () {
     tick();
   }, 500);
 });
